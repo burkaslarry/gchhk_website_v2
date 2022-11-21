648a5f60fbbe6a1fc64af20eb28787f5c2884acd
Larry Lo <>
2022-11-21 21:24:19 +0800

    fix: incorrect padding between terms

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -4,6 +4,7 @@ import HeroBanner from "../components/HeroBanner";
 import TermsSection from "../components/TermsSection";
 import styles from "../styles/Home.module.css";
 import React from "react";
+import { Box } from "@mui/system";
 
 const heroResult = {
   imageUrl: "https://i.imgur.com/p9E5i02.png",
@@ -18,6 +19,10 @@ const Terms: NextPage = () => {
         <HeroBanner resultConfig={heroResult} showButton="false" />
       </section>
       <section id="terms">
+        <Box
+          padding={16}
+          sx={{ width: "100vw", height: "auto", textAlign: "center" }}
+        ></Box>
         <TermsSection
           title="性騷擾指引"
           content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
