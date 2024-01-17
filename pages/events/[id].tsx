2894ea11507b1dab138f2446e07acf3561604d9d
Larry Lo <gaogaigargalden@gmail.com>
2024-01-17 21:06:05 +0800

    feat: handle heading 2 heading 3 item

--- pages/events/[id].tsx
+++ pages/events/[id].tsx
@@ -31,283 +31,289 @@ interface Props {
 }
 
 const actions = [
-  {
-    icon: <HomeOutlinedIcon sx={{ color: "#ffffff" }} />,
-    name: BACK_HOME,
-    key: "home",
-  },
-  {
-    icon: <ContactMailOutlinedIcon sx={{ color: "#ffffff" }} />,
-    name: CONTACT_US,
-    key: "contact",
-  },
-  {
-    icon: <ShareOutlinedIcon sx={{ color: "#ffffff" }} />,
-    name: "分享主頁",
-    key: "share",
-  },
+    {
+        icon: <HomeOutlinedIcon sx={{color: "#ffffff"}}/>,
+        name: BACK_HOME,
+        key: "home",
+    },
+    {
+        icon: <ContactMailOutlinedIcon sx={{color: "#ffffff"}}/>,
+        name: CONTACT_US,
+        key: "contact",
+    },
+    {
+        icon: <ShareOutlinedIcon sx={{color: "#ffffff"}}/>,
+        name: "分享主頁",
+        key: "share",
+    },
 ];
 
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  let { id } = ctx.params as IParams;
-  // Get the dynamic id
+    let {id} = ctx.params as IParams;
+    // Get the dynamic id
 
-  let page_result = await getProject(id);
-  let pageResult = JSON.parse(JSON.stringify(page_result));
-  let bloggerId = pageResult.properties.BlogId.rich_text[0].plain_text;
+    let page_result = await getProject(id);
+    let pageResult = JSON.parse(JSON.stringify(page_result));
+    let bloggerId = pageResult.properties.BlogId.rich_text[0].plain_text;
 
-  let title = pageResult.properties.Title.rich_text[0].plain_text;
+    let title = pageResult.properties.Title.rich_text[0].plain_text;
 
-  let { results } = (await blocks(bloggerId)) as any;
+    let {results} = (await blocks(bloggerId)) as any;
 
-  if (results === undefined) {
-    return {
-      props: {
-        id,
-        post: pageResult,
-        blocks: [],
-        imageGallerySet: [],
-      },
-    };
-  }
-  // Get the children
-
-  var imageSet: string[] = [];
-
-  for (const variable of results) {
-    if (variable.type == "image") {
-      let text = variable["image"].external.url;
-      if (text.length > 0) {
-        imageSet.push(text);
-      }
+    if (results === undefined) {
+        return {
+            props: {
+                id,
+                post: pageResult,
+                blocks: [],
+                imageGallerySet: [],
+            },
+        };
     }
-  }
+    // Get the children
 
-  if (title.includes("(MSW")) {
-    imageSet = []
-    console.log("The title contains '(MSW'");
-  } else {
-    console.log("The title does not contain '(MSW'");
-  }
+    var imageSet: string[] = [];
+
+    for (const variable of results) {
+        if (variable.type == "image") {
+            let text = variable["image"].external.url;
+            if (text.length > 0) {
+                imageSet.push(text);
+            }
+        }
+    }
+
+    // if (title.includes("(MSW")) {
+    //   imageSet = []
+    //   console.log("The title contains '(MSW'");
+    // } else {
+    //   console.log("The title does not contain '(MSW'");
+    // }
 
 
-
-
-  //imageGallerySet;
-  return {
-    props: {
-      id,
-      post: pageResult,
-      blocks: results,
-      imageGallerySet: imageSet,
-    },
-  };
+    //imageGallerySet;
+    return {
+        props: {
+            id,
+            post: pageResult,
+            blocks: results,
+            imageGallerySet: imageSet,
+        },
+    };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  let { results } = await posts();
-  // Get all posts
+    let {results} = await posts();
+    // Get all posts
 
-  return {
-    paths: results.map((post) => {
-      // Go through every post
-      return {
-        params: {
-          // set a params object with an id in it
-          id: post.id,
-        },
-      };
-    }),
-    fallback: false,
-  };
+    return {
+        paths: results.map((post) => {
+            // Go through every post
+            return {
+                params: {
+                    // set a params object with an id in it
+                    id: post.id,
+                },
+            };
+        }),
+        fallback: false,
+    };
 };
 
 const renderBlock = (block: any) => {
 
-  switch (block.type) {
-    case "heading_1":
-      // For a heading
-      return <h1>{block["heading_1"].rich_text[0].plain_text} </h1>;
-    case "paragraph":
-      // For a paragraph
-      let content = block["paragraph"].rich_text[0]?.text?.content || '';
-      console.log("block : " + JSON.stringify(block))
+    switch (block.type) {
+        case "heading_1":
+            // For a heading
+            return <h1>{block["heading_1"].rich_text[0].plain_text} </h1>;
+        case "heading_2":
+            // For a heading
+            return <h2>{block["heading_2"].rich_text[0].plain_text} </h2>;
+        case "heading_3":
+            // For a heading
+            return <h3>{block["heading_3"].rich_text[0].plain_text} </h3>;
+        case "paragraph":
+            // For a paragraph
+            let content = block["paragraph"].rich_text[0]?.text?.content || '';
+            console.log("block : " + JSON.stringify(block))
 
-      let isBold = block["paragraph"].rich_text[0]?.annotations.bold
-      if (isBold) {
-        return (
-              <Typography variant="h6"  sx={{ fontWeight: 'bold' }}>
-                {content}
-              </Typography>             
-        );
-      }  else {
-        return (
-            <Typography variant="h6">
-              {content}
-            </Typography>
-        );
-      }
+            let isBold = block["paragraph"].rich_text[0]?.annotations.bold
+            if (isBold) {
+                return (
+                    <p>
+                      <Typography variant="h6" sx={{fontWeight: 'bold'}}>
+                        {content}
+                      </Typography>
+                    </p>
+                );
+            } else {
+                return (
+                    <Typography variant="h6">
+                        {content}
+                    </Typography>
+                );
+            }
 
-    case "image":
-      // For an image
-      let result = block["image"].external.url;
-      console.log("selected imageresul " + result);
-      return <img alt="" src={result} />;
-    case "bulleted_list_item":
-      // For an unordered list
-       const listItemTextBlockArray = block["bulleted_list_item"].text
-       const listItemRichTextArray = block["bulleted_list_item"].rich_text;
-       var displayText = ""
-      if (Array.isArray(listItemTextBlockArray) && listItemTextBlockArray.length === 0) {
-        // The listItemTextBlockArray is empty
-        displayText = ""
-      } else if (listItemTextBlockArray === undefined) {
-        // The listItemTextBlockArray is undefined
-        displayText = ""
-      } else {
-        displayText = listItemTextBlockArray[0].plain_text
-        // The listItemTextBlockArray is neither empty nor undefined
-      }
+        case "image":
+            // For an image
+            let result = block["image"].external.url;
+            console.log("selected imageresul " + result);
+            return <img alt="" src={result}/>;
+        case "bulleted_list_item":
+            // For an unordered list
+            const listItemTextBlockArray = block["bulleted_list_item"].text
+            const listItemRichTextArray = block["bulleted_list_item"].rich_text;
+            var displayText = ""
+            if (Array.isArray(listItemTextBlockArray) && listItemTextBlockArray.length === 0) {
+                // The listItemTextBlockArray is empty
+                displayText = ""
+            } else if (listItemTextBlockArray === undefined) {
+                // The listItemTextBlockArray is undefined
+                displayText = ""
+            } else {
+                displayText = listItemTextBlockArray[0].plain_text
+                // The listItemTextBlockArray is neither empty nor undefined
+            }
 
-      if (Array.isArray(listItemRichTextArray) && listItemRichTextArray.length === 0) {
-        // The listItemRichTextArray is empty
-        //displayText = ""
-      } else if (listItemRichTextArray === undefined) {
-        // The listItemRichTextArray is undefined
-      } else {
-        displayText = listItemRichTextArray[0].plain_text
-        // The listItemRichTextArray is neither empty nor undefined
-      }
+            if (Array.isArray(listItemRichTextArray) && listItemRichTextArray.length === 0) {
+                // The listItemRichTextArray is empty
+                //displayText = ""
+            } else if (listItemRichTextArray === undefined) {
+                // The listItemRichTextArray is undefined
+            } else {
+                displayText = listItemRichTextArray[0].plain_text
+                // The listItemRichTextArray is neither empty nor undefined
+            }
 
-      return (
-        <ul>
-          <li>{displayText}</li>
-        </ul>
-      );
-    default:
-      // For an extra type
-      return <p></p>;
-  }
+            return (
+                <ul>
+                    <li>{displayText}</li>
+                </ul>
+            );
+        default:
+            // For an extra type
+            return <p></p>;
+    }
 };
 
-const EventPage: NextPage<Props> = ({ id, post, blocks, imageGallerySet }) => {
-  React.useEffect(() => {
-    // Initialise Hotjar only client side
-    hotjar.initialize(3287549, 6);
-    hotjar.stateChange(post.properties.Title.rich_text[0].plain_text);
-  }, []);
+const EventPage: NextPage<Props> = ({id, post, blocks, imageGallerySet}) => {
+    React.useEffect(() => {
+        // Initialise Hotjar only client side
+        hotjar.initialize(3287549, 6);
+        hotjar.stateChange(post.properties.Title.rich_text[0].plain_text);
+    }, []);
 
-  return (
-    <Layout>
-      <section className={styles.banner} id="home">
-        <HeroBanner
-          resultConfig={{
-            imageUrl: post.properties.Gallery.rich_text[0].plain_text,
-            title: post.properties.Title.rich_text[0].plain_text,
-            subtitle: "",
-          }}
-          showButton="false"
-          facebookLink=""
-          igLink=""
-          handleClick={console.log("")}
-        />
-      </section>
-      <div className={styles.blogPageHolder}>
-        <Head>
-          <title>{post.properties.Name.title[0].plain_text}</title>
-        </Head>
-        <div>
-          <br />
+    return (
+        <Layout>
+            <section className={styles.banner} id="home">
+                <HeroBanner
+                    resultConfig={{
+                        imageUrl: post.properties.Gallery.rich_text[0].plain_text,
+                        title: post.properties.Title.rich_text[0].plain_text,
+                        subtitle: "",
+                    }}
+                    showButton="false"
+                    facebookLink=""
+                    igLink=""
+                    handleClick={console.log("")}
+                />
+            </section>
+            <div className={styles.blogPageHolder}>
+                <Head>
+                    <title>{post.properties.Name.title[0].plain_text}</title>
+                </Head>
+                <div>
+                    <br/>
 
-          {blocks.map((block, index) => {
-            return <div key={index}>{renderBlock(block)}</div>;
-          })}
-          <br />
-          <br />
-          <br />
-          <br />
-          <div className="gcccardhk4x">
-            {imageGallerySet.map((imangeLink: string, index) => {
-              return (
-                <div key={index} className="gcccardcanvas">
-                  <a href={imangeLink}>
-                    <img
-                      src={imangeLink}
-                      alt="Landscape picture"
-                      className="imgClass"
-                    />
-                  </a>
+                    {blocks.map((block, index) => {
+                        return <div key={index}>{renderBlock(block)}</div>;
+                    })}
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <div className="gcccardhk4x">
+                        {imageGallerySet.map((imangeLink: string, index) => {
+                            return (
+                                <div key={index} className="gcccardcanvas">
+                                    <a href={imangeLink}>
+                                        <img
+                                            src={imangeLink}
+                                            alt="Landscape picture"
+                                            className="imgClass"
+                                        />
+                                    </a>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-      <SpeedDial
-        ariaLabel="Menu"
-        sx={{
-          position: "fixed",
-          bottom: 24,
-          right: 24,
-          "& .MuiFab-primary": {
-            backgroundColor: "#9926B8",
-            color: "white",
-            width: 64,
-            height: 64,
-            "& .MuiSpeedDialIcon-icon": { fontSize: 32 },
-            "&:hover": { backgroundColor: "#9926B8" },
-          },
-        }}
-        openIcon={<ClearOutlinedIcon />}
-        icon={<MenuOutlinedIcon />}
-      >
-        {actions.map((action) => (
-          <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-            sx={actionSize50}
-            onClick={(e) => {
-              if (action.key == "contact") {
-                if (typeof window === "undefined") return null;
+            </div>
+            <SpeedDial
+                ariaLabel="Menu"
+                sx={{
+                    position: "fixed",
+                    bottom: 24,
+                    right: 24,
+                    "& .MuiFab-primary": {
+                        backgroundColor: "#9926B8",
+                        color: "white",
+                        width: 64,
+                        height: 64,
+                        "& .MuiSpeedDialIcon-icon": {fontSize: 32},
+                        "&:hover": {backgroundColor: "#9926B8"},
+                    },
+                }}
+                openIcon={<ClearOutlinedIcon/>}
+                icon={<MenuOutlinedIcon/>}
+            >
+                {actions.map((action) => (
+                    <SpeedDialAction
+                        key={action.name}
+                        icon={action.icon}
+                        tooltipTitle={action.name}
+                        sx={actionSize50}
+                        onClick={(e) => {
+                            if (action.key == "contact") {
+                                if (typeof window === "undefined") return null;
 
-                Router.push({
-                  pathname: "/",
-                  query: { action: "contact" },
-                });
-              } else if (action.key == "home") {
-                if (typeof window === "undefined") return null;
+                                Router.push({
+                                    pathname: "/",
+                                    query: {action: "contact"},
+                                });
+                            } else if (action.key == "home") {
+                                if (typeof window === "undefined") return null;
 
-                Router.back();
-              } else if (action.key == "share") {
-                // Check for Web Share api support
-                if (navigator.share) {
-                  // Browser supports native share api
-                  navigator
-                    .share({
-                      text: "草根文化館致力促進教育、保護環境、救助貧困",
-                      url: Router.pathname,
-                    })
-                    .then(() => {
-                      console.log("Thanks for sharing!");
-                    })
-                    .catch((err) => {
-                      console.error(err)
-                      alert(SHARE_NOT_SUPPORTED);
-                    } );
-                } else {
-                  // Fallback
-                  alert(SHARE_NOT_SUPPORTED);
-                }
-              }
-            }}
-          />
-        ))}
-      </SpeedDial>
-      <Analytics/>
-    </Layout>
-  );
+                                Router.back();
+                            } else if (action.key == "share") {
+                                // Check for Web Share api support
+                                if (navigator.share) {
+                                    // Browser supports native share api
+                                    navigator
+                                        .share({
+                                            text: "草根文化館致力促進教育、保護環境、救助貧困",
+                                            url: Router.pathname,
+                                        })
+                                        .then(() => {
+                                            console.log("Thanks for sharing!");
+                                        })
+                                        .catch((err) => {
+                                            console.error(err)
+                                            alert(SHARE_NOT_SUPPORTED);
+                                        });
+                                } else {
+                                    // Fallback
+                                    alert(SHARE_NOT_SUPPORTED);
+                                }
+                            }
+                        }}
+                    />
+                ))}
+            </SpeedDial>
+            <Analytics/>
+        </Layout>
+    );
 };
 
 export default EventPage;
